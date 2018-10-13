15b6abea1cd1147b4a50fe1d6faa98151e06bec9
Manpreet Singh <33646286+ManpreetSingh007@users.noreply.github.com>
2018-10-13 22:56:05 +0530

    Add files via upload

---
+++ NQueens - Exercise 17 - Manpreet/src/nQueens/TestMainline.java
@@ -0,0 +1,14 @@
+package nQueens;
+
+import java.util.ArrayList;
+
+public class TestMainline {
+
+	public static void main(String[] args) {
+		System.out.println("Author - Manpreet Singh");
+		GameBoard Test5By5 = new GameBoard(5);
+		Test5By5.place(2, 2);
+		System.out.println(Test5By5);
+	}
+
+}
